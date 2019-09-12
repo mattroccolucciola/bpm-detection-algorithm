056346e2d250a0eff3d508dd16f372d5b0cab3c4
mattrlucciola <mr3lucciola@gmail.com>
2019-09-12 07:51:00 -0400

    pull mp3 buffer completed

--- README.md
+++ README.md
@@ -46,7 +46,7 @@ TIMELINE
 | HTML Design      |    L     |    30 Min      |    20 min     | -10 min  |
 | CSS Structuring  |    L     |    30 Min      |    15 min     | |
 | JS for base site |    L     |    30 Min      |     0 min     | |
-| Pull MP3 - algo  |    H     |   120 Min      |   175 min     | |
-| Analyze BPM algo |    H     |   360 Min      |     0 min     | |
+| Pull MP3 - algo  |    H     |   120 Min      |   355 min     | |
+| Analyze BPM algo |    H     |   360 Min      |   120 min     | |
 | Genre label algo |    L     |   120 Min      |     0 min     | |
 | Genre BPM algo   |    L     |    90 Min      |     0 min     | |
--- retrieve_mp3.js
+++ retrieve_mp3.js
@@ -1,13 +1,11 @@
 track_id = 170734376;
 CLIENT_ID = '6aSX01kZxpetA85mf5R9Ezqs3ozjO2zc';
 
-let trackPerma = 'https://soundcloud.com/petitbiscuit/we-were-young-robotaki-remix'
-
-
 // process input from user and parse & clean it
 function mungUserInput() {
     let submitField = document.querySelector('#url');
     let userInput = submitField.value;
+
     // check if its a permalink or full URL
     if (userInput.includes('soundcloud.com/')) {
         urlString = userInput;
@@ -20,21 +18,16 @@ function mungUserInput() {
     return urlString
 }
 
+// get the asset from servers
 async function pullMp3(url) {
     info = await axios.get(url);
     streamURL = info['data']['stream_url'] + `?client_id=${CLIENT_ID}`;
-    streamData = await axios.get(streamURL);
+    return await axios.get(streamURL);
     return streamData['request']['responseURL'];
 }
 
-/*
-https://soundcloud.com/petitbiscuit/we-were-young-robotaki-remix
-*/
 // get the mp3
-async function getMP3 (event) {
-    // prevent default
-    event.preventDefault();
-
+async function getMP3Path () {
     // delete this line, its for testing only
     document.querySelector('#url').value = 'https://soundcloud.com/petitbiscuit/we-were-young-robotaki-remix';
     // document.querySelector('#url').value = 'https://soundcloud.com/inspected/sam-gellaitry-waiting-so-long';
@@ -44,54 +37,182 @@ async function getMP3 (event) {
 
     // get the track id via the main url
     let fullUrl = `https://api.soundcloud.com/resolve?url=${trackURL}&client_id=${CLIENT_ID}`
-    
+
     // mung the page source, get the url for making api call
-    mp3URL = await pullMp3(fullUrl);
-    console.log(mp3URL);
-    console.log('hi');
-    
-    
-    
+    streamData = await pullMp3(fullUrl);
+    return streamData
+}
+
+function createBuffers(node) {
+    var context = window.AudioContext || window.webkitAudioContext
+    context = new context
+    request = new XMLHttpRequest();
+    request.open('GET', node['request']['responseURL'], true);
+    request.responseType = 'arraybuffer';
+    request.onload = function() {
+        let audioData = request.response;
+        context.decodeAudioData(audioData, function(buffer) {
+            dogBarkingBuffer = buffer;
+            console.log('buffer');
+            console.log(buffer);
+        });
+    }
+    request.send();
 
 
 
 
 
-    // test (below)
-    
-    // test (above)
 
 
 
 
 
 
-    // let trackIDRequest = await axios.get(trackURL, {
-    //         headers: {
-                
-    //         },
+
+
+
+
+
+
+
+    // var context = window.AudioContext || window.webkitAudioContext
+    // // Fetch Audio Track via AJAX with URL
+    // request = new XMLHttpRequest();
+    // await request.open('GET', url, true);
+    // request.responseType = 'arraybuffer';
+    // request.onload = async function() {
+    //     let audioData = request.response;
+    //     console.log(request);
+        
+    //     console.log(audioData);
+        
+        
+    //     await context.decodeAudioData(audioData, function(buffer) {
+    //         dogBarkingBuffer = buffer;
     //     });
-    // console.log(trackIDRequest);
-    
+    // }
+    // request.send();
 
-    let info = {};
-    // let response;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // request.onload = function(ajaxResponseBuffer) {
+   
+    //     // Create and Save Original Buffer Audio Context in 'originalBuffer'
+    //     var AudioContext = window.AudioContext || window.webkitAudioContext
+    //     var OfflineAudioContext = window.OfflineAudioContext || window.webkitOfflineAudioContext
+        
+    //     var audioCtx = new AudioContext();
+        
+    //     var songLength = ajaxResponseBuffer.total;
     
-    // response = await axios.get(
-    //     `"https://api.soundcloud.com/tracks/${track_id}?client_id=${CLIENT_ID}"`
-    // );
-    // console.log(response);
-    // return response
+    //     // Arguments: Channels, Length, Sample Rate
+    //     var offlineCtx = new OfflineAudioContext(1, songLength, 44100);
+    //     source = offlineCtx.createBufferSource();
+        
+    //     var audioData = request.response;
+    //     console.log('audioData');
+    //     console.log(ajaxResponseBuffer);
+    //     audioCtx.decodeAudioData(audioData, function(buffer) {
+    
+    //         window.originalBuffer = buffer.getChannelData(0);
+    //         var source = offlineCtx.createBufferSource();
+    //         source.buffer = buffer;
+
+    //         // Create a Low Pass Filter to Isolate Low End Beat
+    //         var filter = offlineCtx.createBiquadFilter();
+    //         filter.type = "lowpass";
+    //         filter.frequency.value = 140;
+    //         source.connect(filter);
+    //         filter.connect(offlineCtx.destination);
+    //         console.log(offlineCtx);
+            
+    //         // Render this low pass filter data to new Audio Context and Save in 'lowPassBuffer'
+    //         offlineCtx.startRendering().then(function(lowPassAudioBuffer) {
+
+    //             var audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+    //             var song = audioCtx.createBufferSource();
+    //             song.buffer = lowPassAudioBuffer;
+    //             song.connect(audioCtx.destination);
+
+    //             // Save lowPassBuffer in Global Array
+    //             window.lowPassBuffer = song.buffer.getChannelData(0);
+    //             console.log("Low Pass Buffer Rendered!");
+    //         });
+
+    //     },
+    //     function(e) {});
+    // }
+    // request.send();
+}
+    
+   
+   
+
+function analyzeMP3BPM(mp3Path) {
+    // Create offline context
+    var offlineContext = new OfflineAudioContext(1, 1000000, 44100);
+
+    // Create buffer source
+    var source = offlineContext.createBufferSource();
+    source.buffer = buffer;
+
+    // Create filter
+    var filter = offlineContext.createBiquadFilter();
+    filter.type = "lowpass";
+
+    // Pipe the song into the filter, and the filter into the offline context
+    source.connect(filter);
+    filter.connect(offlineContext.destination);
+
+    // Schedule the song to start playing at time:0
+    source.start(0);
+
+    // Render the song
+    offlineContext.startRendering()
+
+    // Act on the result
+    offlineContext.oncomplete = function(e) {
+        // Filtered buffer!
+        var filteredBuffer = e.renderedBuffer;
+    };
+}
+
+// full function
+async function getAndAnalyzeMP3 (event) {
+    // prevent default
+    event.preventDefault();
+
+    // get the mp3 path
+    streamData = await getMP3Path();
+    console.log(streamData);
+    
+    // analyze the bpm
+    // mp3Path = './waiting-so-long.mp3'
+    createBuffers(streamData);
+    // bpm = analyzeMP3BPM(mp3Path);
 };
-
-
 
 // main function
 function main() {
     let submitButton = document.querySelector('#getURL');
 
     // the entire script basically needs to be assigned to the submit button
-    submitButton.addEventListener('click', getMP3)
+    submitButton.addEventListener('click', getAndAnalyzeMP3)
     
 }
 
