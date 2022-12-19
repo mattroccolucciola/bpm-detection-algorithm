bcd75461d017961dfa6fbe6562530e7c2b00cf8b
mrlucciola <47836741+mrlucciola@users.noreply.github.com>
2022-12-19 13:34:13 -0500

    style the data display and text

--- src/components/Home/SongDetailDisplay.tsx
+++ src/components/Home/SongDetailDisplay.tsx
@@ -2,7 +2,7 @@
 import { observer } from "mobx-react-lite";
 import { useHomeContext } from "./mobx";
 // mui
-import { Stack, Paper } from "@mui/material";
+import { Stack, Paper, Box } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
 import { TG } from "../../mui/Utils";
 import { SProps } from "../../mui/interfaces";
@@ -10,7 +10,7 @@ import { SProps } from "../../mui/interfaces";
 import SongInfo from "./SongInfo";
 
 /** # Contains song info returned from the call by `<SongInput />`
- * 
+ *
  * @todo:
  * 1. Display a default `<Paper />` element with text to indicate no content
  * 1. Display an error occured while fetching - toast noti
@@ -18,13 +18,21 @@ import SongInfo from "./SongInfo";
  * 1. Add embed element
  */
 const SongDetailDisplay: React.FC<SProps> = () => {
+  // state
   const title: string = useHomeContext((s) => s.songMetrics.title);
+  const artworkUrl: string = useHomeContext((s) => s.songMetrics.artwork_url);
 
   return (
-    <Stack direction="column" component={Paper}>
-      <TG>{title}</TG>
-      <SongInfo />
-      <Grid className="embed"></Grid>
+    <Stack direction="column" component={Box} p={1}>
+      {title && (
+        <>
+          <Stack flexDirection="row" p={1}>
+            <img src={artworkUrl} alt="" />
+            <TG p={1}>{title}</TG>
+          </Stack>
+          <SongInfo />
+        </>
+      )}
     </Stack>
   );
 };
--- src/components/Home/SongInfo.tsx
+++ src/components/Home/SongInfo.tsx
@@ -1,5 +1,5 @@
 // style
-import { Divider, Stack } from "@mui/material";
+import { Divider, Stack, Paper, Box } from "@mui/material";
 import { SProps } from "../../mui/interfaces";
 import { TG } from "../../mui/Utils";
 import { useHomeContext } from "./mobx";
@@ -13,44 +13,99 @@ interface CategoryMapping {
   title: string;
 }
 // constants
-const categories = new Map<string, CategoryMapping>([
-  ["artwork_url", { key: "artwork_url", title: "" }],
-  ["duration", { key: "duration", title: "Duration" }],
-  ["genre", { key: "genre", title: "Genre" }],
-  ["release_date", { key: "release_date", title: "Release Date" }],
-  ["playback_count", { key: "playback_count", title: "Plays" }],
-  ["likes_count", { key: "likes_count", title: "Likes" }],
-  ["reposts_count", { key: "reposts_count", title: "Reposts" }],
-  ["comment_count", { key: "comment_count", title: "Comments" }],
-]);
+const categoriesArr: CategoryMapping[] = [
+  { key: "duration", title: "Duration" },
+  { key: "genre", title: "Genre" },
+  { key: "release_date", title: "Release Date" },
+  { key: "playback_count", title: "Plays" },
+  { key: "likes_count", title: "Likes" },
+  { key: "reposts_count", title: "Reposts" },
+  { key: "comment_count", title: "Comments" },
+];
 
+const calcDaysAgo = (dateStr: string): string => {
+  const date: Date = new Date(dateStr);
+  const today: Date = new Date();
+  // millisecond difference
+  const deltaMs = today.getTime() - date.getTime();
+  const deltaMin = deltaMs / 1000 / 60;
+  const deltaDays = deltaMin / (60 * 24);
+  return `${Math.round(deltaDays).toString()} days ago`;
+};
 /** # Input field for song URL */
 const SongInfoItem: React.FC<{
   category: CategoryMapping;
 }> = ({ category }) => {
-  const categoryData = useHomeContext((s) => s.songMetrics[category.key]);
+  let categoryData: string | number = useHomeContext(
+    (s) => s.songMetrics[category.key]
+  );
+  if (category.key === "duration") {
+    const date = new Date("0");
+    date.setMilliseconds(categoryData as number);
+    let categoryStr: string = new Date(categoryData as number)
+      .toISOString()
+      .substring(11, 19);
+    categoryStr =
+      categoryStr[0] === "0" ? categoryStr.substring(1) : categoryStr;
+    categoryData =
+      categoryStr[0] === "0" ? categoryStr.substring(2) : categoryStr;
+  } else if (
+    [
+      "playback_count",
+      "likes_count",
+      "reposts_count",
+      "comment_count",
+    ].includes(category.key)
+  ) {
+    categoryData = (categoryData as number).toLocaleString("en-US");
+  }
 
   return (
-    <Stack direction="column">
-      <TG>{category.title}</TG>
-      <Divider orientation="horizontal" />
-      <TG>{categoryData}</TG>
+    <Stack
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="flex-end"
+      py={1}
+      px={3}
+    >
+      {category.key === "release_date" ? (
+        <Stack flexDirection="column" alignItems="center">
+          <TG>{new Date(categoryData).toDateString()}</TG>
+          <TG>{calcDaysAgo(categoryData as string)}</TG>
+        </Stack>
+      ) : (
+        <TG>{categoryData}</TG>
+      )}
+      <Divider
+        flexItem
+        orientation="horizontal"
+        sx={{ paddingTop: "5px", marginBottom: "5px" }}
+      />
+      <TG fontSize="0.6em">{category.title}</TG>
     </Stack>
   );
 };
 
 const SongInfo: React.FC<SProps> = () => {
-  const artworkUrl:string = useHomeContext(s=>s.songMetrics.artwork_url);
   // create the list of elements
   const categoryElems: React.ReactElement<{ categoryInfo: InfoGroup }>[] = [];
-  categories.forEach((category: CategoryMapping, idx: string) => {
+  categoriesArr.forEach((category: CategoryMapping, idx: number) => {
     categoryElems.push(<SongInfoItem category={category} key={idx} />);
+    if (idx < categoriesArr.length - 1) {
+      categoryElems.push(
+        <Divider
+          orientation="vertical"
+          key={`div-${idx}`}
+          flexItem
+          variant="middle"
+        />
+      );
+    }
   });
 
   return (
-    <Stack direction="row">
-      <img src={artworkUrl} alt="" />
-      <Stack direction="row">{categoryElems}</Stack>
+    <Stack direction="row" p={1} justifyContent="center">
+      {categoryElems}
     </Stack>
   );
 };
